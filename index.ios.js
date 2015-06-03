fd531c245b406580095a587e0205bacb641165d1
Les Orchard <me@lmorchard.com>
2015-06-03 16:43:31 -0400

    Initial attempt at stealing TodoMVC models and writing web & native React views

--- index.ios.js
+++ index.ios.js
@@ -1,7 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
 'use strict';
 
 var React = require('react-native');
@@ -10,28 +6,12 @@ var {
   StyleSheet,
   Text,
   View,
+  ListView,
 } = React;
 
 var _ = require('lodash');
 
-var NativeApp = React.createClass({
-  render: function() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
-});
+var TodosCollection = new (require('./lib/models/todos'))();
 
 var styles = StyleSheet.create({
   container: {
@@ -50,6 +30,84 @@ var styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
+  listView: {
+    paddingTop: 20,
+    backgroundColor: '#f5fcff'
+  }
+});
+
+var TodoList = React.createClass({
+
+  render() {
+    return (
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={(item) => <TodoItem item={item} />}
+        style={styles.listView}
+      />
+    );
+  },
+
+  getInitialState() {
+    return {
+      collection: this.props.collection,
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2
+      })
+    };
+  },
+
+  componentDidMount() {
+    // TODO: Should this happen in getInitialState instead?
+    this.setState({ dataSource: this.updateDataSource() });
+
+    this.state.collection.on('add remove change', () => {
+      this.setState({ dataSource: this.updateDataSource() });
+    });
+  },
+
+  componentWillUnmount() {
+	  this.state.collection.off(null, null, this);
+  },
+
+  updateDataSource() {
+    // Make a clone of the collection model set
+    // TODO: Is there a better way to do this?
+    var rows = this.state.collection.map((model) => model);
+    return this.state.dataSource.cloneWithRows(rows);
+  }
+
+});
+
+AppRegistry.registerComponent('TodoList', () => TodoList);
+
+var TodoItem = React.createClass({
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>{this.state.item.title} - {this.state.item.completed ? 'true' : 'false'}</Text>
+      </View>
+    );
+  },
+  getInitialState() {
+    return { item: this.props.item };
+  },
+  componentWillReceiveProps(props) {
+    this.setState({ item: props.item });
+  }
+});
+
+AppRegistry.registerComponent('TodoItem', () => TodoItem);
+
+var NativeApp = React.createClass({
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>TODO LIST</Text>
+        <TodoList collection={TodosCollection} />
+      </View>
+    );
+  }
 });
 
 AppRegistry.registerComponent('NativeApp', () => NativeApp);
--- index.web.js
+++ index.web.js
@@ -1,26 +1,48 @@
 var React = require('react');
 
-var _ = require('lodash');
+var TodosCollection = new (require('./lib/models/todos'))();
 
-var Child = React.createClass({
-  render: function(){
+var TodoList = React.createClass({
+  render() {
+    var items = this.state.collection.map((item) => {
+      return ( <TodoItem item={item} />);
+    });
+    return ( <ul>{items}</ul>);
+  },
+  getInitialState() {
+    return { collection: this.props.collection };
+  },
+  componentDidMount() {
+	  this.state.collection.on('add remove change', () => this.forceUpdate());
+  },
+  componentWillUnmount() {
+	  this.state.collection.off(null, null, this);
+  }
+});
+
+var TodoItem = React.createClass({
+  render() {
+    return (
+      <li>{this.state.item.title} - {this.state.item.completed ? 'true' : 'false'}</li>
+    );
+  },
+  getInitialState() {
+    return { item: this.props.item };
+  },
+  componentWillReceiveProps(props) {
+    this.setState({ item: props.item });
+  }
+});
+
+var App = React.createClass({
+  render() {
     return (
       <div>
-        and this is the <b>{this.props.name}</b>.
+        <h1>TODOs</h1>
+        <TodoList collection={TodosCollection} />
       </div>
     )
   }
 });
 
-var Parent = React.createClass({
-  render: function(){
-    return (
-      <div>
-        <div> This is the parent. </div>
-        <Child name="child"/>
-      </div>
-    )
-  }
-});
-
-React.render(<Parent />, document.getElementById('app'));
+React.render(<App />, document.getElementById('app'));
